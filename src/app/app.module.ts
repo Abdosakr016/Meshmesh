8c27998e20ad1f64c63aaacd7f1b2c915ee5ff70
abdos016 <Abdosakrdev016@gmail.com>
2023-09-26 15:41:58 +0300

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './product-list/product-list.component';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { CardComponent } from './card/card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CardModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ApiServiceService } from '../service/api-service.service';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  seller :any;
+  constructor(private  ApiServiceService:ApiServiceService ) { }
+
+  ngOnInit() {
+    this.ApiServiceService.getProductList().subscribe(((data)=>this.seller=data)
+    );
+  }
+}

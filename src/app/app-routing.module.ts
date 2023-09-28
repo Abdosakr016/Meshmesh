498dbbd2609047bf609d73713e9447db626fe08b
magdaahmed2 <magda.ahmed2162000@gmail.com>
2023-09-28 09:49:23 +0300

    register and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ChooseFormComponent } from './choose-form/choose-form.component';
+import { RegisterClinicComponent } from './register-clinic/register-clinic.component';
 
-const routes: Routes = [];
+  const routes: Routes = [
+    {
+      path: 'register',
+      component: RegisterComponent,
+      
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+    {
+      path: 'choose-form',
+      component: ChooseFormComponent,
+    },
+    {
+      path: 'register-clinic',
+      component: RegisterClinicComponent,
+    },
+    
+     
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ChooseFormComponent } from './choose-form/choose-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterClinicComponent } from './register-clinic/register-clinic.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ChooseFormComponent,
+    RegisterClinicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+   ReactiveFormsModule,
+   FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

9c4771eac44bc6d02debe535cfb1c209c9ec59d2
khalidgomaa <khaled.gomaagedo@gmail.comgit config --global user.email khaled.gomaagedo@gmail.com>
2023-10-02 22:25:36 +0300

    after adding doctors list

--- src/app/porto_veterinary/components/creat-list-doctors/creat-list-doctors.component.ts
+++ src/app/porto_veterinary/components/creat-list-doctors/creat-list-doctors.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-creat-list-doctors',
   templateUrl: './creat-list-doctors.component.html',
   styleUrls: ['./creat-list-doctors.component.css']
 })
-export class CreatListDoctorsComponent {
-  
+export class CreatListDoctorsComponent implements OnInit {
   doctorForm!: FormGroup;
-  doctorsFormArray: any
-  i=5;
-  onSubmit(){}
+  doctorForms: FormGroup[] = [];
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.initializeDoctorForm();
+  }
+
+  initializeDoctorForm() {
+    this.doctorForm = this.fb.group({
+      name: ['', Validators.required],
+      photo: ['',Validators.required]
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission here
+  }
+
+  addDoctorForm() {
+    this.doctorForms.push(this.fb.group({
+      name: ['', Validators.required],
+      photo: ['',Validators.required]
+    }));
+  }
 }

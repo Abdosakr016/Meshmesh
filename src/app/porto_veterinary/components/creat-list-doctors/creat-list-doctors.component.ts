1bbd5dc24f363758a477fa86113db126a86d5c20
magdaahmed2 <magda.ahmed2162000@gmail.com>
2023-11-02 11:12:40 +0200

    magda

--- src/app/porto_veterinary/components/creat-list-doctors/creat-list-doctors.component.ts
+++ src/app/porto_veterinary/components/creat-list-doctors/creat-list-doctors.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,NgZone } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VeterinaryService } from '../../services/veterinary.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-creat-list-doctors',
@@ -8,23 +9,61 @@ import { VeterinaryService } from '../../services/veterinary.service';
   styleUrls: ['./creat-list-doctors.component.css']
 })
 export class CreatListDoctorsComponent implements OnInit {
+  [x: string]: any;
   doctorForm!: FormGroup;
   doctorForms: FormGroup[] = [];
   doctorBase64:any;
-  arrDoctors:any;
-  constructor(private fb: FormBuilder,private VetService:VeterinaryService) { }
+  updateDoctorForm!:FormGroup;
+  arrDoctors:any=[];
+  getId:any;
+  currentDoctor:any;
+  constructor(private fb: FormBuilder,private VetService:VeterinaryService
+    ,private router:Router,private activateroute:ActivatedRoute)
+    {
+      
+      // Initialize your form group (updateDoctorForm) here
+    this.updateDoctorForm = this.fb.group({
+      name: [''],
+      image: ['', Validators.required],
+      experience: ['', Validators.required],
+    });
+  }
+
+  // Function to set the current doctor data for update
+  setUpdateData(doctor: any) {
+    this.currentDoctor = doctor;
+    // Bind the data to the form controls
+    this.updateDoctorForm.patchValue({
+      name: doctor.name,
+      experience: doctor.experience,
+       image: doctor.image,
+      // You can handle the 'image' control as needed, e.g., displaying the current image
+      
+    });
+    
+  }
+  
+   
 
   ngOnInit() {
     this.initializeDoctorForm();
-    this.VetService.get_doctors().subscribe(((data)=>this.arrDoctors=data))
+    this.VetService.get_doctors().subscribe(res=>{
+      console.log(Object.values(res)[0]);
+      this.arrDoctors=Object.values(res)[0];
+    })
   }
+
 
   initializeDoctorForm() {
     this.doctorForm = this.fb.group({
       name: ['', Validators.required],
-      photo: ['',Validators.required]
+      image: ['',Validators.required],
+      experience: ['',Validators.required],
+      
     });
   }
+
+
 
   get_doctorImagepath(event: any){
     const file=event.target.files[0]
@@ -38,10 +77,30 @@ export class CreatListDoctorsComponent implements OnInit {
   onAddDoctor() {
     if (this.doctorForm.valid) {
       const doctorData = this.doctorForm.value;
+      
+      // Update the data using the API service
+      this.VetService.addNewDoctor(doctorData).subscribe(
+
+        (response) => {
+         
+          console.log('Data updated successfully:', response);
+          
+        },
+        (error: any) => {
+          console.error('Error updating data:', error);
+        }
+      );
+    }
+  }
+
+
+  onUpdateDoctor() {
+    if (this.updateDoctorForm.valid) {
+      const doctorData = this.updateDoctorForm.value;
       console.log(doctorData);
   
       // Update the data using the API service
-      this.VetService.addNewDoctor(doctorData).subscribe(
+      this.VetService.updatDoctor(this.getId,doctorData).subscribe(
         (response) => {
           console.log('Data updated successfully:', response);
           
@@ -56,20 +115,42 @@ export class CreatListDoctorsComponent implements OnInit {
 
   
   
-  deleteDoctor(doctor_id: number) {
-   
-    const doctor_id_str = doctor_id.toString();
+  // deleteDoctor(id:any,i:any) {
+  //  console.log(id);
+       
+  //   this.VetService.deleteDoctor(id).subscribe(res =>{
+  //     this.arrDoctors.splice(i,1);
+  //   })
+    
+  //    }
   
+  
+  // deleteDoctor(id: any) {
+  //   // Find the index of the doctor with the specified 'id' in the arrDoctors array
+  //   const index = this.arrDoctors.findIndex((doctor: { id: any }) => doctor.id === id);
+  
+  //   if (index !== -1) {
+  //     // Delete the doctor on the server using your service method
+  //     this.VetService.deleteDoctor(id).subscribe((res) => {
+  //       // Remove the doctor from the arrDoctors array
+  //       this.arrDoctors.splice(index, 1);
+  //     });
+  //   }
+  // }
+  
+  deleteDoctor(id: number) {
    
-    this.VetService.deleteDoctor(doctor_id_str).subscribe(
+    const id_str = id.toString();
+    this.VetService.deleteDoctor(id_str).subscribe(
       (response) => {
         console.log('doctor deleted successfully:', response);
-  
- 
       },
       (error) => {
         console.error('Error deleting doctor:', error);
       }
     );
   }
-}
+
+    
+  }
+
